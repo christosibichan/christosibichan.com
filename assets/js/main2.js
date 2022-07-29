d5f692d0f022a244e56ae2357e39d8ff35f121bf
Christo Sibichan <90307986+christosibichan@users.noreply.github.com>
2022-07-29 23:38:47 +0530

    Update main2.js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -11,11 +11,20 @@ async function handleSubmit(event) {
 		'Accept': 'application/json'
 	}
   }).then(response => {
-	status.innerHTML = "Thanks for your submission!";
-	form.reset()
+	if (response.ok) {
+	  status.innerHTML = "Thanks for your submission!";
+	  form.reset()
+	} else {
+	  response.json().then(data => {
+		if (Object.hasOwn(data, 'errors')) {
+		  status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+		} else {
+		  status.innerHTML = "Oops! There was a problem submitting your form"
+		}
+	  })
+	}
   }).catch(error => {
 	status.innerHTML = "Oops! There was a problem submitting your form"
   });
 }
 form.addEventListener("submit", handleSubmit)
-
